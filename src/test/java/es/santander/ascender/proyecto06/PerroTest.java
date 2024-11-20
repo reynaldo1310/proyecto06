a7a119a927ca9454bd091cb7210a637bdf1b9dfc
curso09 <curso09@gmail.com>
2024-11-20 14:01:01 +0100

    Continuar con el ejercicio , hemos arreglado PerroTest

--- src/test/java/es/santander/ascender/proyecto06/PerroTest.java
+++ src/test/java/es/santander/ascender/proyecto06/PerroTest.java
@@ -2,6 +2,10 @@ package es.santander.ascender.proyecto06;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import org.junit.jupiter.api.Test;
 
 public class PerroTest {
@@ -13,6 +17,39 @@ public class PerroTest {
         perro.comer(34.2);
 
         assertEquals(79.7, perro.getPeso(), 0.000001);
+    }
+
+    @Test 
+    void TestCompararColeccionesVerdad() {
+
+
+
+    }
+
+    @Test
+    void TestCompararColeccionesFalse() {
+
+    }
+
+    public boolean comparar(List<Perro> lista1, List<Perro> lista2) {
+        if (lista1 == null && lista2 == null){
+            return true;
+        } else if(lista1 == null || lista2 == null) {
+            
+        }
+        if (lista1 == null || lista2 == null) {
+            return false;
+        }
+
+        if(lista1.size() !=  lista2.size()) {
+            return false;
+        }
+        for (int i = 0; i < lista1.size(); i++) {
+            if (! Objects.equals(lista1.get(i), lista2.get(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
